c7c9338301136467878378a1097a7557407b5b87
landakdu <durilandak429@gmail.com>
2024-09-26 15:57:15 +0700

    Add files via upload

---
+++ htmlforcall.js
@@ -0,0 +1,22 @@
+// code standing inside html.js for easy edit
+const menu1 = `
+<button class="tutup" onClick="window.location.reload();">TUTUP CHAT</button>
+`;
+
+const menu2 = `
+<img class="iklan1" onclick="document.location='../../index.html'" style="background-image: url(https://iili.io/dL4LHyx.gif);"></img>
+`;
+
+const menu3 = `
+<img class="iklan2" onclick="document.location='../../index.html'" style="background-image: url(https://iili.io/dL6Jeje.gif);"></img>
+`;
+
+const menu4 = `
+<button class="buka" onclick="enableAutoplay(), showhide(), tombol()" value="button"><a class="buka1"><span>MULAI CHAT</span></a></button>
+<button class="buka0" onclick="document.location='../../index.html'" value="button">JUDISUHU</button>
+`;
+
+const menu5 = `
+
+`;
+
