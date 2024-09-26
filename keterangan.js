0f110e9691d057878e7f9b1186f3354cd4c41b27
landakdu <durilandak429@gmail.com>
2024-09-26 16:07:24 +0700

    Add files via upload

---
+++ keterangan.js
@@ -0,0 +1,19 @@
+const bgbahaya = document.getElementsByClassName("bgbahaya");
+for (let i = 0; i < bgbahaya.length; i++) {
+  bgbahaya[i].innerHTML = "<h1 class='keterangan-font-size'><span class='color-red'>SITUS INI BERBAHAYA</span>, sebab tidak dapat di temukan kemungkin telah tutup ataupun berganti nama, di takutkan ini adalah situs scam yang dapat menghilang kapan saja dan merubah nama situs kapan saja, hal ini sangat berbahaya dampaknya untuk pemain dan untuk mencegah hal yang tidak di inginkan lebih lanjut kami tidak mencantumkan link ataupun chat situs ini demi menjaga kenyamanan anda atas perhatiannya kami ucapkan terima kasih 🙏.</h1>";
+}
+
+const bgwaspada = document.getElementsByClassName("bgwaspada");
+for (let i = 0; i < bgwaspada.length; i++) {
+  bgwaspada[i].innerHTML = "<h1 class='keterangan-font-size'>Mohon maaf situs ini belum di nyatakan aman oleh kami karena beberapa alasan, <span class='color-yellow'>ada yang karena belum kami coba</span><span class='color-red'>, ada juga situs tidak menjaga keamanan akun pemain</span> contoh cs akan meresetkan password akun anda untuk siapa saja yang datang ke livechat dengan memberikan data rekening anda hal ini akan memudahkan siapa saja melakukan hack akun anda dan data nomor rekening sangat mudah di dapatkan dari penjual data base, pinjol, cek nomor secara acak di banking, admin nakal situs lain,  atau siapa saja yang mengetahui nomor rekening anda intinya situs tidak menjaga keamanan akun anda<span class='color-red'>, ada juga karena situs tidak membayar kemenangan</span><span class='color-yellow'>, ada juga situs tidak memberikan hak pemain seperti yang di janjikan</span><span class='color-red'>, intinya situs ini beresiko untuk pemain</span>, apabila tetap ingin melanjutkan masuk ke situs ini silahkan gunakan <span class='color-red'>TOMBOL CHAT</span> di bawah untuk berkomunikasi meminta link secara langsung ke situsnya, atas perhatiannya kami ucapkan terima kasih 🙏.</h1>";
+}
+
+const bgdomain = document.getElementsByClassName("bgdomain");
+for (let i = 0; i < bgdomain.length; i++) {
+  bgdomain[i].innerHTML = "<h1 class='keterangan-font-size'>Tombol berwarna <span class='color-green'>HIJAU</span> artinya situs tersebut <span class='color-green'>AMAN</span> dan link tanpa vpn dapat di gunakan selamanya karena otomatis update setiap hari<br/><br/>Tombol berwarna <span class='color-yellow'>KUNING</span> dan <span class='color-red'>MERAH</span> artinya situs tersebut tidak aman menurut judisuhu dan link tanpa vpn tidak di sediakan judisuhu, jika membutuhkan link terbaru klik tombol <span class='color-red'>CHAT</span> yang di sediakan judisuhu agar anda dapat berkomunikasi dan meminta link terbaru secara langsung ke situs resminya, tombol <span class='color-red'>CHAT</span>  hanya muncul jika anda mengklik tombol <span class='color-yellow'>KUNING</span> atau<span class='color-red'>MERAH</span>sesuai dengan situs yang anda inginkan.</h1>";
+}
+
+const bgchatadmin = document.getElementsByClassName("bgchatadmin");
+for (let i = 0; i < bgchatadmin.length; i++) {
+  bgchatadmin[i].innerHTML = "<h1 class='keterangan-font-size'>CHAT KE ADMIN JUDISUHU</br></br><span class='color-blue'>CHAT FACEBOOK :</span> di gunakan untuk berkomunikasi dengan admin dan akan di balas saat admin online.</br></br><span class='color-green'>CHAT TAWK :</span> di gunakan untuk mengirimkan pesan singkat untuk admin tanpa membutuhkan balasan chat dari admin biasanya di pergunakan untuk request situs yang belum tersedia atau meminta admin mengupdate link baru</br></br>Contoh 1 pesan singkat</br>admin update link terbaru situs ABCD</br></br>Contoh 2 pesan singkat</br>admin tambahkan situs 1234 agar saya dapat masuk situsnya tanpa vpn selamanya</br></br>Intinya sampaikan saja seluruh keinginan anda dalam 1 kali pengiriman pesan nanti jika sudah di baca oleh admin akan di kerjakan.</h1>";
+}
